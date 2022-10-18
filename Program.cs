15e6d3464e096676c97783bc5ce964b22121a6fb
Andrea Burr <burrandy18@gmail.com>
2022-10-18 14:32:59 -0700

    mini6redo

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// Andrea Burr 
+// 10-18-2022
+// mini Odd or Even redo - github
+Console.Clear();
+
+Console.WriteLine("Lets compare odd or even ");
+Console.WriteLine("enter your number");
+
+bool playagain = true;
+
+while (playagain){
+    
+
+    
+string userinput = Console.ReadLine();
+
+int intOne = int.Parse(userinput);
+    if (intOne % 2 == 0){
+        Console.WriteLine("this is an even number");
+        Console.WriteLine(intOne);
+    }else{
+        Console.WriteLine("this is an odd number");
+        Console.WriteLine(intOne);
+       
+
+}
+Console.WriteLine("if you are done playing type no or press any key to continue");
+string contPlay = Console.ReadLine();
+
+  if (contPlay == "no"){
+
+ playagain = false; 
+  }else{
+    playagain = true;
+  }
+
+}
+
+
+
